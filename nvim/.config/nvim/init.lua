21998fb8c778e8b9f91b73b54990b885822f57d0
Gj1337 <valentyn.hlyvich@gmail.com>
2025-11-12 18:42:17 +0200

    Nvim change dark theme to oldworld

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,7 @@
 require("config.lazy")
 require("keymaps.lsp")
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+-- vim.opt.number = true
+-- vim.opt.relativenumber = true
 vim.o.signcolumn = "yes"
 vim.o.winborder = 'rounded'
--- nvim/.config/nvim/lua/plugins/colorsheme.lua
+++ nvim/.config/nvim/lua/plugins/colorsheme.lua
@@ -1,42 +1,46 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      local function is_dark_mode()
-        -- macOS
-        local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
-        if handle then
-          local result = handle:read("*a")
-          handle:close()
-          if result:match("Dark") then
-            return true
-          end
-        end
+	{
+		"dgox16/oldworld.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local function is_dark_mode()
+				-- macOS
+				local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+				if handle then
+					local result = handle:read("*a")
+					handle:close()
+					if result:match("Dark") then
+						return true
+					end
+				end
+				-- GNOME
+				handle = io.popen("gsettings get org.gnome.desktop.interface color-scheme 2>/dev/null")
+				if handle then
+					local result = handle:read("*a")
+					handle:close()
+					return result:match("dark") ~= nil
+				end
+				return false
+			end
 
-        -- GNOME
-        handle = io.popen("gsettings get org.gnome.desktop.interface color-scheme 2>/dev/null")
-        if handle then
-          local result = handle:read("*a")
-          handle:close()
-          return result:match("dark") ~= nil
-        end
+			if is_dark_mode() then
+				require("oldworld").setup({ transparent = true })
+				vim.cmd.colorscheme("oldworld")
+			else
+				require("github-theme").setup({
+					options = { transparent = true },
+				})
+				vim.cmd.colorscheme("github_light")
+			end
 
-        return false
-      end
-
-      if is_dark_mode() then
-        require("tokyonight").setup({ transparent = true })
-        vim.cmd.colorscheme("tokyonight-night")
-      else
-        require("github-theme").setup({
-          options = { transparent = true },
-        })
-        vim.cmd.colorscheme("github_light")
-      end
-    end,
-  },
-  { "projekt0n/github-nvim-theme", lazy = true },
+			-- Ensure transparency is applied after colorscheme loads
+			vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+			vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+		end,
+	},
+	{ "projekt0n/github-nvim-theme",    lazy = true },
+	{ "rockerBOO/boo-colorscheme-nvim", lazy = false },
 }
-
